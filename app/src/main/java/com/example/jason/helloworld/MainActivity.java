cfebc81e61675e3b06a6fc27c582a4028a2b466f
jrjacobson <jrjacobson86@gmail.com>
2015-02-04 08:42:33 -0700

    Initial commit

---
+++ app/src/main/java/com/example/jason/helloworld/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.jason.helloworld;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import java.sql.SQLOutput;
+
+import static com.example.jason.helloworld.R.id.textView2;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void sayHi(View view) {
+        TextView txt = (TextView)findViewById(textView2);
+        txt.setText("Hello World!");
+        System.out.println("Hello World!");
+
+    }
+    public void clear(View view) {
+        TextView txt = (TextView)findViewById(textView2);
+        txt.setText("");
+    }
+}
